815436f2cea040d1f505755bf63d137970d25164
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-08 15:10:09 +0530

    Create basic1.java

---
+++ basic1.java
@@ -0,0 +1,16 @@
+public class basic1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a>b&&b<a);
+        a+=b;
+        System.out.println(b%a!=0||a>b);
+        b+=2;
+        System.out.println(b%a!=0);
+        b/=a;
+        System.out.println(a%2!=0||b%2==0);
+        b+=a;
+        System.out.println(a%2!=0||b%2==0);
+
+    }
+}
