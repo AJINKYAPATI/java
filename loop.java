89e82ad05447d949042aaf7a389b13e6bc53820a
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-12 14:34:07 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,12 @@
+class loop {
+    
+    static int b;
+    static String s;
+
+
+    public static void main(String []args) {
+
+        System.out.println(b);
+        System.out.println(s);
+    } 
+}
