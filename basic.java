2c381a88f5c017e7e31b12cf735c27177368dd6d
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-03 23:21:57 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -2,7 +2,6 @@ class basic{
     public static void main(String[] args) {
          int a=10;
         int b=20;
-
         a+=20;
         b+=a;
         System.out.println(b);
@@ -12,6 +11,5 @@ class basic{
         System.out.println(b);
         b%=10;
         System.out.println(a+b);
-
 }
 }
